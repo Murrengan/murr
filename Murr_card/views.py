bfc177d26ff19174823983ed7f2311646eac3409
murrengan <murrengan@yandex.ru>
2019-03-30 12:34:59 +0300

    add all categories count

--- Murr_card/views.py
+++ Murr_card/views.py
@@ -1,4 +1,5 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import Count
 from django.shortcuts import render, get_object_or_404
 
 from .forms import CommentForm
@@ -6,6 +7,7 @@ from .models import Murr
 
 
 def murrs_list(requset):
+    all_categories_count = get_all_categories_count()[0:5]
     all_murrs = Murr.objects.filter().order_by('-timestamp')
     paginator = Paginator(all_murrs, 2)
     page_request_ver = 'page'
@@ -22,6 +24,7 @@ def murrs_list(requset):
         'murrs': paginator_queriset,
         'page_request_ver': page_request_ver,
 
+        'all_categories_count': all_categories_count,
         'latest': latest
     }
     return render(requset, 'Murr_card/murr_list.html', context)
@@ -41,3 +44,9 @@ def murr_detail(request, pk):
         'form': form
     }
     return render(request, 'Murr_card/murr_detail.html', context)
+
+
+def get_all_categories_count():
+    # Получаем Имя значения values('categories__title') и их колличество (categories__title отправляем к модели)
+    all_categories_count = Murr.objects.values('categories__title').annotate(Count('categories__title'))
+    return all_categories_count
