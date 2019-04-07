8b418bfc146cc9daf39bc70b45acb15e3e1a845c
dipperside <figarich@gmail.com>
2019-04-08 02:19:36 +0300

    Добавил комментариям всплывающий футер с контролами управдения

--- murr/settings.py
+++ murr/settings.py
@@ -105,7 +105,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-RU'
 
 TIME_ZONE = 'UTC'
 
--- static/js/custom.js
+++ static/js/custom.js
@@ -1,3 +1,11 @@
+// Установка csrf_token
+(function () {
+    let csrftoken = Cookies.get('csrftoken');
+    $.ajaxSetup({
+        headers: {"X-CSRFToken": csrftoken}
+    });
+})();
+
 // Добавляем плавную прокрутку к элементу
 $('.nav-link, .navbar-brand').click(function() {
   $('.navbar-collapse').collapse('toggle'); // При нажатии на ссылку, меню сворачивается
@@ -5,4 +13,59 @@ $('.nav-link, .navbar-brand').click(function() {
   $('html, body').animate({
     scrollTop: $(sectionTo).offset().top -56 // Отступ от блока (Обычно задается высота nav-bar)
   }, 1000); // Время прокрутки до элемента в секундах
+});
+
+
+// Показать форму комментария
+let openForm = function (id) {
+    $(`#${id}`).show()
+};
+// Скрыить форму комментария
+let closeForm = function (id) {
+    $(`#${id}`).hide()
+};
+// Поставить лайк
+let like = function (id) {
+    $.ajax({
+        url: "http://127.0.0.1:8000/like/",
+        type: "POST",
+        data: {
+            pk: id,
+        },
+        success: (response) => {
+            window.location = response
+        },
+        error: (response) => {
+            console.log("False")
+        }
+    })
+};
+// Подписаться
+let follow = function (id) {
+    $.ajax({
+        url: "http://127.0.0.1:8000/profile/follow/",
+        type: "POST",
+        data: {
+            pk: id,
+        },
+        success: (response) => {
+            window.location = response
+        },
+        error: (response) => {
+            console.log("False")
+        }
+    })
+};
+// обработать форму авторизации с помощью ajax request.
+$(".need_auth").submit(function (e) {
+    e.preventDefault();
+    var url = $(this).attr('action');
+    var data = $(this).serialize();
+    $.post(
+        url,
+        data,
+        function (response) {
+            window.location = response.location;
+        },
+    );
 });
