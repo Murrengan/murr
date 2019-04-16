9a993f8ed00c835956fcf100e9aff8c02f3de69c
dipper <figarich@gmail.com>
2019-04-16 17:46:52 +0300

    murr author icon on header, link to profile, tags in footer

--- static/js/custom.js
+++ static/js/custom.js
@@ -7,12 +7,12 @@
 })();
 
 // Добавляем плавную прокрутку к элементу
-$('.nav-link, .navbar-brand').click(function() {
-  $('.navbar-collapse').collapse('toggle'); // При нажатии на ссылку, меню сворачивается
-  var sectionTo = $(this).attr('href');
-  $('html, body').animate({
-    scrollTop: $(sectionTo).offset().top -56 // Отступ от блока (Обычно задается высота nav-bar)
-  }, 1000); // Время прокрутки до элемента в секундах
+$('.nav-link, .navbar-brand').click(function () {
+    $('.navbar-collapse').collapse('toggle'); // При нажатии на ссылку, меню сворачивается
+    var sectionTo = $(this).attr('href');
+    $('html, body').animate({
+        scrollTop: $(sectionTo).offset().top - 56 // Отступ от блока (Обычно задается высота nav-bar)
+    }, 1000); // Время прокрутки до элемента в секундах
 });
 
 
@@ -33,12 +33,12 @@ let like = function (id) {
             pk: id,
         },
         success: (response) => {
-            window.location = response
-        },
-        error: (response) => {
-            console.log("False")
-        }
-    })
+        window.location = response
+},
+    (response) =;> {
+        console.log("False")
+    }
+})
 };
 // Подписаться
 let follow = function (id) {
@@ -49,12 +49,12 @@ let follow = function (id) {
             pk: id,
         },
         success: (response) => {
-            window.location = response
-        },
-        error: (response) => {
-            console.log("False")
-        }
-    })
+        window.location = response
+},
+    (response) =;> {
+        console.log("False")
+    }
+})
 };
 // обработать форму авторизации с помощью ajax request.
 $(".need_auth").submit(function (e) {
@@ -70,66 +70,67 @@ $(".need_auth").submit(function (e) {
     );
 });
 
-
 $(".edt-comment").on('click', function () {
     // console.log('reply');
     msg = $(this).parents("div.media-body").find('p').html();
-    if ( $('#id_content').val() ) {
-      $('#id_content').val("");
+    if ($('#id_content').val()) {
+        $('#id_content').val("");
     }
     $('#id_content').val(msg);
     $('#id_content').focus();
 
-    console.log(msg);
-    return false;});
+    console.log(msg, $(this).parent('.comment-control.small').data('id'));
+    return false;
+});
 
 $('.del-comment').on('click', function () {
-    a=$(this).parents('.media-body').find('.comm-content').data('id');
-    console.log(a)
+    id = $(this).parent('.comment-control.small').data('id');
+    var commentRow = $(this).parent('.media.m-2');
+    console.log('will be delete comment id -'+id);
     BootstrapDialog.show({
-          title: 'Подтвердите действие',
-          type: 'type-danger',
-          message: 'Вы уверены, что хотите удалить этот комментарий?',
-          buttons: [{
-              label: 'Удалить',
-              cssClass: 'btn-primary btn-sm',
-              action: function(dialog) {
+        title: 'Подтвердите действие',
+        type: 'type-danger',
+        message: 'Вы уверены, что хотите удалить этот комментарий?',
+        buttons: [{
+            label: 'Удалить',
+            cssClass: 'btn-primary btn-sm',
+            action: function (dialog) {
                 $.ajax({
-                  type: 'POST',
-                  dataType: 'json',
-                  url: '/murrs/murr_detail/comment_cut/'+a+'/',
-                  data: {id_comment: a},
-                  success: function(response) {
-                    if (response.success) {
-                      //актуализируем кол-во комментариев
-                      var commentCount = parseInt($('#commentsCounter').html());
-                      $('#commentsCounter').html(commentCount - 1);
+                    type: 'POST',
+                    dataType: 'json',
+                    url: '/murrs/murr_detail/comment_cut/' + a + '/',
+                    data: {id_comment: id},
+                    success: function (response) {
+                        if (response.success) {
+                            //актуализируем кол-во комментариев
+                            var commentCount = parseInt($('#commentsCounter').html());
+                            $('#commentsCounter').html(commentCount - 1);
 
-                      commentRow.animate({
-                        opacity: 1,
-                        height: 0,
-                        padding: 0
-                      }, 'fast', function(){
-                        commentRow.remove();
-                      });
+                            commentRow.animate({
+                                opacity: 1,
+                                height: 0,
+                                padding: 0
+                            }, 'fast', function () {
+                                commentRow.remove();
+                            });
+                        }
+                        else {
+                            alert('Внимание ' + response.message);
+                        }
+                    },
+                    error: function () {
+                        alerrt('Сервер не отвечает. Попробуйте повторить позднее.');
                     }
-                    else {
-                      alert('Внимание '+response.message);
-                    }
-                  },
-                  error: function() {
-                    alerrt('Сервер не отвечает. Попробуйте повторить позднее.');
-                  }
                 });
                 dialog.close();
-              }
-          }, {
-              label: 'Отмена',
-              cssClass: 'btn-sm',
-              action: function(dialog) {
+            }
+        }, {
+            label: 'Отмена',
+            cssClass: 'btn-sm',
+            action: function (dialog) {
                 dialog.close();
-              }
-          }]
-        });
+            }
+        }]
+    });
     return false;
 });
