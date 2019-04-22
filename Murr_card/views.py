12c705272de6b4344a484ea3d342422d5243a8c1
das-dev <33005044+das-dev@users.noreply.github.com>
2019-04-22 22:38:51 +0300

    fix search (#35)
    fix pagination on the founded murrs list
    fix search result for case when nothing found
    minor refactoring

--- Murr_card/views.py
+++ Murr_card/views.py
@@ -1,12 +1,11 @@
-from taggit.models import Tag
-
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.db.models import Q, Count
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, JsonResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse
+from taggit.models import Tag
 
 from .forms import CommentForm, MurrForm
 from .models import Murr, MurrVisiting, Comment, Category
@@ -15,24 +14,29 @@ User = get_user_model()
 
 
 def murr_list(request, **kwargs):
-    """ в kwargs передавать tag_name - для отбора по тегам;
-    search_result - отбора по результатам поиска"""
+    """ Output all murrs or murrs that filtered by tag
+        or murrs queryset from kwargs"""
 
-    all_murrs = Murr.objects.filter(is_public=True, is_draft=False)
+    murrs = Murr.objects.filter(is_public=True, is_draft=False)
+
     if request.user.is_authenticated:
         # all murrs + my drafts
-        all_murrs = Murr.objects.filter(Q(is_draft=True, author_id=request.user.id) | Q(is_draft=False))
+        availabled = Q(is_draft=True, author_id=request.user.id) | Q(is_draft=False)
+        murrs = Murr.objects.filter(availabled)
 
     tag_name = kwargs.get('tag_name')
     if tag_name:
         tag = get_object_or_404(Tag, name=tag_name)
-        all_murrs = all_murrs.filter(tags__name=tag)
+        murrs = murrs.filter(tags__name=tag)
 
-    if kwargs.get('search_result'):
-        all_murrs = kwargs.get('search_result')
+    found_murrs = kwargs.get('search_result')
+    search_query = kwargs.get('search_query', '')
+    if found_murrs is not None:
+        murrs = found_murrs
+        search_query = f'q={search_query}&'
 
-    all_murrs = all_murrs.annotate(comments_total=Count('comments__pk')).order_by('-timestamp')
-    paginator = Paginator(all_murrs, 5)
+    murrs = murrs.annotate(comments_total=Count('comments__pk')).order_by('-timestamp')
+    paginator = Paginator(murrs, 5)
     try:
         page = paginator.page(request.GET.get('page'))
     except PageNotAnInteger:
@@ -40,8 +44,12 @@ def murr_list(request, **kwargs):
     except EmptyPage:
         page = paginator.page(paginator.num_pages)
 
-    context = {'murrs': page, 'last_two': all_murrs[:2], 'categories': Category.objects.all()}
-    return render(request, 'Murr_card/murr_list.html', context)
+    context = {
+        'page': page,
+        'search_query': search_query,
+        'categories': Category.objects.all(),
+    }
+
 
 
 def murr_detail(request, slug):
@@ -52,27 +60,25 @@ def murr_detail(request, slug):
         form.instance.murr = murr
         form.save()
         return HttpResponseRedirect(request.path)
+
     if request.user.is_authenticated and request.user.pk != murr.author_id:
         MurrVisiting.objects.get_or_create(user=request.user, murr=murr)
 
-    return render(request, 'Murr_card/murr_detail.html', {'murr': murr, 'form': form})
+    context = {'murr': murr, 'form': form}
+    return render(request, 'Murr_card/murr_detail.html', context)
 
 
 def search(request):
-    queryset = ''
-    all_murrs = Murr.objects.all()
+    """Filter murrs by search query and pass queryser to murr_list view"""
+
+    murrs = Murr.objects.none()
     query = request.GET.get('q')
     if query:
-        queryset = all_murrs.filter(
-            Q(title__icontains=query) |
-            Q(description__icontains=query)
-        ).distinct()
+        search_fields = Q(title__icontains=query) | Q(description__icontains=query)
+        murrs = Murr.objects.filter(search_fields).distinct()
 
-    context = {
-        'search_result': queryset
-    }
+    return murr_list(request, search_result=murrs, search_query=query)
 
-    return murr_list(request, **context)
 
 
 @login_required
@@ -87,10 +93,8 @@ def murr_create(request):
             return redirect(reverse('murr_detail', kwargs={
                 'slug': form.instance.slug
             }))
-    context = {
-        'title': title,
-        'form': form
-    }
+
+    context = {'title': title, 'form': form}
     return render(request, 'Murr_card/murr_create.html', context)
 
 
@@ -110,14 +114,13 @@ def murr_update(request, slug):
             return redirect(reverse('murr_detail', kwargs={
                 'slug': form.instance.slug
             }))
-    context = {
-        'title': title,
-        'form': form
-    }
+
+    context = {'title': title, 'form': form}
     return render(request, template, context)
 
 
 def murr_delete(request, slug):
+    ''' удалить мурр '''
     murr = get_object_or_404(Murr, slug=slug)
     murr.delete()
     return redirect(reverse('murr_list'))
@@ -128,3 +131,26 @@ def comment_cut(request, id):
     # comment.delete()
     print(f'\n\n{comment} --------------- were here\n\n')
     return redirect(reverse('murr_list'))
+
+
+def comment_cut(request, id):
+    ''' удалить комментарий '''
+    comment = get_object_or_404(Comment, pk=id)
+    comment.delete()
+    return JsonResponse({'success': True})
+
+
+def comment_edit(request, id):
+    ''' изменить комментарий'''
+    data = dict()
+    comment = get_object_or_404(Comment, pk=id)
+    # comment.delete()
+    # render_to_string
+    return JsonResponse({'success': True})
+
+
+def comment_reply(request, id):
+    ''' ответ на комментарий'''
+    comment = get_object_or_404(Comment, pk=id)
+    # comment.delete()
+    return JsonResponse({'success': True})
