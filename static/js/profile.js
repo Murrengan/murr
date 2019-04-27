e96dc5f0eafa2e25404c5b43624332ed87a73331
dasdev <das.dev@pm.me>
2019-04-27 12:12:22 +0700

    add skeleton for js
    add kernel.js with $.namespace func
    add profile.js for murren's profile page
    
    inspired by https://habr.com/ru/post/158235/

---
+++ static/js/profile.js
@@ -0,0 +1,14 @@
+;(function($) {
+  let $name = 'murrenProfile',
+      methods = {
+        init: function() {
+          return this.each(function() {
+            let $this = $(this);
+
+          })
+        },
+        follow: function() {
+        }
+      };
+  $.fn[$name] = $.namespace(methods)
+})(jQuery);
