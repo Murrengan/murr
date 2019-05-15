20b57dc3bbfdc212be67037e33eb0ab26948ace8
Dobrodiy Igor <figarich@gmail.com>
2019-05-15 21:06:39 +0300

    remove tiny from comment adding form (#61)
    * [~] gradient to overflowed murl_ist card
    
    * [*] restore default textarea input for adding comments

--- MurrCard/forms.py
+++ MurrCard/forms.py
@@ -13,18 +13,20 @@ class TinyMCEWidget(TinyMCE):
 
 
 class CustomCheckbox(Field):
-    template = 'MurrCard/custom_checkbox.html'
+    template = 'Murr_card/custom_checkbox.html'
 
 
 class MurrForm(forms.ModelForm):
     content = forms.CharField(
         widget=TinyMCEWidget(
-            attrs={'required': False, 'cols': 30, 'rows': 10},
+            attrs={'required': False, 'cols': 30, 'rows': 15},
             mce_attrs=({'menubar': False,
-                        'plugins': ['advlist autolink lists link image charmap print preview anchor textcolor',
-                                    'searchreplace visualblocks code fullscreen',
-                                    'insertdatetime media table contextmenu paste code help wordcount',
-                                    'autoresize'],
+                        'plugins': ['advlist autolink lists link image imagetools charmap print preview anchor',
+                                    'textcolor searchreplace visualblocks code fullscreen insertdatetime media',
+                                    'table contextmenu paste code help wordcount autoresize',
+                                    ],
+                        'autoresize_min_height': 250,
+                        'autoresize_on_init': False,
                         'toolbar': '''
                                 insert | undo redo |  formatselect | bold italic backcolor  | 
                                 alignjustify | bullist numlist outdent indent | 
@@ -32,7 +34,8 @@ class MurrForm(forms.ModelForm):
                                 ''',
                         'toolbar2': '''''',
                         'content_css': ['//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
-                                        '//www.tinymce.com/css/codepen.min.css'],
+                                        'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css'],
+                        'content_css_cors': 'true',
                         'branding': False,
                         'content_style': 'img {max-width: 100%; height:auto;}',
                         'imagetools_toolbar': "rotateleft rotateright | flipv fliph | editimage imageoptions",
@@ -85,26 +88,26 @@ class MurrForm(forms.ModelForm):
 
 
 class CommentForm(forms.ModelForm):
-    # content = forms.CharField(widget=forms.Textarea(
-    #     attrs={'class': 'form-control',
-    #            'placeholder': 'введите ваш комментарий',
-    #           'rows': '4', }
-# ))
+    content = forms.CharField(widget=forms.Textarea(
+        attrs={'class': 'form-control', 'placeholder': 'введите ваш комментарий','rows': '4',}
+    ),
+        label="Написать комментарий"
+    )
 
     class Meta:
         model = Comment
         fields = ['content']
-        widgets = {
-            'content': TinyMCEWidget(attrs={
-                'class': 'form-control',
-                'placeholder': 'введите ваш комментарий',
-                'rows': '4',
-                'id': 'id_content',
-            }),
-        }
-        labels = {
-            "content": "Написать комментарий"
-        }
+        # labels = {
+        #     "content": "Написать комментарий",
+        # }
+        # widgets = {
+        #     'content': TinyMCEWidget(attrs={
+        #         'class': 'form-control',
+        #         'placeholder': 'введите ваш комментарий',
+        #         'rows': '4',
+        #         'id': 'id_content',
+        #     }),
+        # }
 
 
 class CommentEditForm(forms.ModelForm):
