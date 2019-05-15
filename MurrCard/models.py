84282ab075961f355d414300495e6874f2b7b299
Heckad <heckad@yandex.ru>
2019-05-15 12:40:14 +0300

    Remove second cover_url function

--- MurrCard/models.py
+++ MurrCard/models.py
@@ -56,11 +56,6 @@ class Murr(models.Model):
     def get_liked_murrens(self):
         return self.liked.values_list('murren_id', flat=True)
 
-    @property
-    def cover_url(self):
-        if self.cover and hasattr(self.cover, 'url'):
-            return self.cover.url
-
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
         if self.cover:
