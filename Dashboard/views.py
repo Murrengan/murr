270c1a117d1f3e748199783277bcef821928ab68
Murrengan <40840064+Murrengan@users.noreply.github.com>
2019-05-26 09:42:53 +0300

    Egor murren (#70)
    * Show in dashboards murrs by follower master
    
    * fix for murr in popup

--- Dashboard/views.py
+++ Dashboard/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 
-from Murren.models import Murren
+from MurrCard.models import Murr
 
 
 def dashboard(request):
-    murrens = Murren.objects.filter(followers__follower__pk=request.user.pk)
+
+    murrs = Murr.objects.filter(author__followers__follower__pk=request.user.pk)
+
     context = {
-        'murrens': murrens
+        'murrs': murrs
     }
+
     return render(request, 'Dashboard/dashboard.html', context)
--- MurrCard/urls.py
+++ MurrCard/urls.py
@@ -10,9 +10,9 @@ urlpatterns = [
     path('murrs_by_category/<str:category>', murr_card.murr_list, name='murr_list_by_category'),
     path('create/', murr_card.murr_create, name='murr_create'),
     # murr_detail under construction: нужно переверстать старый шаблон с учетом новых инклюдови и апи вьъх
-    # path('murr_detail/<str:slug>', murr_card.murr_detail, name='murr_detail'),
-    # path('murr_detail/<str:slug>/update/', murr_card.murr_update, name='murr_update'),
-    # path('murr_detail/<str:slug>/delete/', murr_card.murr_delete, name='murr_delete'),
+    path('murr_detail/<str:slug>', murr_card.murr_detail, name='murr_detail'),
+    path('murr_detail/<str:slug>/update/', murr_card.murr_update, name='murr_update'),
+    path('murr_detail/<str:slug>/delete/', murr_card.murr_delete, name='murr_delete'),
     path('comment_add/', murr_card.comment_add),
     path('comment_delete/', murr_card.comment_delete),
     path('comment_edit/', murr_card.comment_edit),
