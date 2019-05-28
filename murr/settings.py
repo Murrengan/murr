b151593ce9817b69f8040906ea6f62f4fb6b9668
dasdev <das.dev@pm.me>
2019-05-29 01:46:19 +0700

    [+] add more readable and scalable categories bar

--- murr/settings.py
+++ murr/settings.py
@@ -65,7 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'Dashboard.context_processors.show_categories',
+                'murr.context_processors.show_categories',
             ],
         },
     },
