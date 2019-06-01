2e4e5604b425d9617acaeceb50e3e4f216d907a0
das-dev <33005044+das-dev@users.noreply.github.com>
2019-06-01 14:26:20 +0700

    [+/-] rename urls for murr list (#81)
    and drop Murr#get_murrs_by_category because it is incorrect and not used;

--- murr/context_processors.py
+++ murr/context_processors.py
@@ -6,7 +6,7 @@ from MurrCard.models import Murr
 def show_categories(request):
     is_search = '/murrs/' in request.path
     is_murr_list = request.path == '/murrs/'
-    is_murrs_by_categories = '/murrs/murrs_by_category/' in request.path
+    is_murrs_by_categories = '/murrs/by_category/' in request.path
 
     if not any([is_search, is_murr_list, is_murrs_by_categories]):
         return {}
