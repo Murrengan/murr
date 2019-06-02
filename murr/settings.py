892e762e9889201696e502fc12d99606aa49cea0
Asja Makarowska <anutya@mail.ru>
2019-06-02 15:04:25 +0300

    Добавлен дефолтный контекст процессор для csrf токена Поправлено название токена на фронте

--- murr/settings.py
+++ murr/settings.py
@@ -66,6 +66,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'murr.context_processors.show_categories',
+                'django.template.context_processors.csrf',
             ],
         },
     },
