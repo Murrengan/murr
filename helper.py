3d857f4aaf3daef3e6d3e5d6b88b6db94d326f93
murrengan <murrengan@yandex.ru>
2019-07-17 09:16:20 +0300

    add helper

---
+++ helper.py
@@ -0,0 +1,5 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('test1', '', 'test1')"
+
