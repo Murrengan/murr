15cb4d974581b1d92ee659253e29fe2b1240d8fc
murrengan <murrengan@yandex.ru>
2019-09-03 22:03:04 +0300

    add vue cli

---
+++ frontend/murr_api/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+
+    <HelloWorld msg_variable="Murrengan"/>
+
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+
+
+</style>
---
+++ frontend/murr_api/src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+
+    <h1>{{ msg_variable }}</h1>
+
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg_variable: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
