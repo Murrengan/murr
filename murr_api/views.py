6e22701c050e2c4333b943b4e275141cda8b8422
Egor <Egor.Komarov@acronis.com>
2019-09-13 17:58:09 +0300

    update murr_api and navbar

--- murr_api/views.py
+++ murr_api/views.py
@@ -14,29 +14,74 @@ class MurrenList(generics.ListAPIView):
 
 
 class MurrenById(generics.RetrieveAPIView):
-
     queryset = Murren.objects.all()
     serializer_class = MurrenSerializer
 
 
 class MurrList(generics.ListCreateAPIView):
-
     queryset = Murr.objects.all()
     serializer_class = MurrSerializer
 
 
 class MurrDetail(generics.RetrieveUpdateDestroyAPIView):
-
     permission_classes = (IsAuthorOrReadOnly,)
     queryset = Murr.objects.all()
     serializer_class = MurrSerializer
 
 
 def start(request):
-    data = {'base_card_img_url': 'http://127.0.0.1:8000/static/img/murr_game/Gorbug.png'}
+    text = """""<br>
+                    <span class="hell-font">"Оставьте всякую надежду, вы, входящие"</span><br><br>
+
+                    Горбаг - город на границе у врат ада.<br><br>
+
+                    Первая территория, куда попадают души.<br>
+                    Путники наполняют таверны золотом.<br>
+                    Теплый кров спасает от ужаса ночи.<br>
+                    Надежда еще жива...<br><br>
+
+                    Я стою перед открытой дверью в красную таверну.<br><br>
+                    Запах вкусной курочки доносится из шумного здания.<br>
+                    """""
+    show_btn = [
+        {
+            'btb': 'show_hell_gate__btn',
+            'btn_text': 'Взглянуть на ворота',
+            'btn_color': '#14c8ff',
+            'def_on_click': 'look_at_hell_gate',
+
+        },
+
+        {
+            'btb': 'show_tawern_card__btn',
+            'btn_text': 'Войти в таверну',
+            'btn_color': '#ff91fb',
+            'def_on_click': 'come_to_tawern',
+        }
+
+    ]
+
+    data = {'base_card_img_url': '', 'base_card_text': text, 'show_btn': show_btn}
     return JsonResponse(data, status=200)
 
 
 def hell_gate(request):
-    data = {'base_card_img_url': 'http://127.0.0.1:8000/static/img/murr_game/hell_gate.png'}
+    text = """""<br>
+        Врата ада<br>
+        и жарко и холодно.<br>
+        Я представлял их по другому.<br><br>
+        
+        Их образ всегда перед моими глазами.<br>
+        Душа наполняется трепетом и горем.<br>
+        характеристики -5<br><br>
+
+
+        <span class="hell-font">"Lasciate ogni speranza, voi ch’entrate"</span><br>
+
+        """""
+    show_btn = ['show_come_to_tawern__btn']
+
+    data = {'base_card_img_url': 'http://127.0.0.1:8000/static/img/murr_game/hell_gate.png',
+            'base_card_text': text,
+            'show_btn': show_btn}
     return JsonResponse(data, status=200)
