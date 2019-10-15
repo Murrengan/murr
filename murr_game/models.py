68189ef0abcddd365e44921bd264c2cf6edbd342
Egor <Egor.Komarov@acronis.com>
2019-10-15 17:05:19 +0300

    update

--- murr_game/models.py
+++ murr_game/models.py
@@ -4,8 +4,40 @@ from django.db import models
 User = get_user_model()
 
 
-class Character(models.Model):
-    name = models.CharField(max_length=25, verbose_name='Name', help_text="Имя", unique=True)
-    actions = models.CharField(max_length=256, verbose_name='Actions', help_text="Действия")
-    stats = models.CharField(max_length=256, verbose_name='Stats', help_text="Статы")
-    character_db = models.ForeignKey(User, on_delete=models.CASCADE, related_name='character_db', blank=True, null=True)
+class MurrenGameData(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='murren_game_data')
+    hp = models.IntegerField(default=60)
+    mp = models.IntegerField(default=20)
+    lvl = models.IntegerField(default=1)
+    exp = models.IntegerField(default=0)
+    locations = models.CharField(max_length=255)
+
+    strength = models.IntegerField(default=5)
+    vitality = models.IntegerField(default=5)
+    dexterity = models.IntegerField(default=5)
+    intelligence = models.IntegerField(default=5)
+    luck = models.IntegerField(default=1)
+
+    inventory = models.ManyToManyField('Inventory')
+    skill = models.ManyToManyField('Skill')
+    armory = models.ManyToManyField('Armory')
+
+
+class Inventory(models.Model):
+    name = models.CharField(max_length=30)
+    value = models.IntegerField(default=0)
+    description = models.CharField(max_length=255)
+
+
+class Skill(models.Model):
+    name = models.CharField(max_length=30)
+    mp_value = models.IntegerField(default=0)
+    description = models.CharField(max_length=255)
+
+
+class Armory(models.Model):
+    name = models.CharField(max_length=30)
+    attack = models.IntegerField(default=0)
+    defence = models.IntegerField(default=0)
+    buff = models.IntegerField(default=0, help_text='Изменяет базовые характеристики')
+    description = models.CharField(max_length=255)
