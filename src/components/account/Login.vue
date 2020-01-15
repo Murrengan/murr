603c2936fef3f253f788c67b19e0999489da1bf1
ilya vegner <ilyavegner@mrX.Dlink>
2020-01-15 21:10:30 +0300

    Added handler error

--- src/components/account/Login.vue
+++ src/components/account/Login.vue
@@ -4,20 +4,33 @@
 
         <h1 class="h1">Войти</h1>
 
+        <div class="erorrHandling" v-if="isError">
+            {{ errorText }}
+        </div>
+
         <form class="form"
               v-on:submit.prevent="murrenLogin">
 
-            <input type="text"
+            <!-- <input type="text"
                    v-model="murrenName"
-                   placeholder="Имя Муррена">
+                   placeholder="Имя Муррена"> -->
 
+            <MurrInput  
+                type="text" 
+                placeholder="Имя Муррена" 
+                :isError="error_container.hasOwnProperty('username')"
+                :errorText=" error_container.hasOwnProperty('username') ? error_container.username : null " 
+                @input="val => murrenName = val" />
             <br>
 
-            <input type="password"
-                   v-model="password"
-                   placeholder="Пароль">
+            <MurrInput  
+                type="password" 
+                placeholder="Пароль" 
+                :isError="error_container.hasOwnProperty('password')"
+                :errorText=" error_container.hasOwnProperty('password') ? error_container.password : null " 
+                @input="val => password = val" />
 
-            <br><br>
+            <br>
 
             <button type="submit">
                 Погнали</button>
@@ -30,9 +43,14 @@
 
 <script>
 
-    import axios from 'axios'
+    import axios from 'axios';
+    import MurrInput from '@/components/Input';
 
     export default {
+
+        components: {
+            MurrInput
+        },
 
         data() {
 
@@ -40,31 +58,64 @@
 
                 murrenName: '',
                 password: '',
+
+                isError: false,
+                errorText: null,
+
+                error_container: {},
             }
         },
 
         methods: {
 
             async murrenLogin() {
+                this.error_container = {};
+                this.isError = false;
+                this.errorText = null;
 
-                let cred = {
+                if (this.murrenName !== '' && this.password !== '') {
+                    let cred = {
 
                     username: this.murrenName,
                     password: this.password,
-                };
+                    };
 
-                let tokens = await axios.post('/murren/token_create/', cred);
+                    try {
+                        let tokens = await axios.post('/murren/token_create/', cred);
+                        
+                        this.isError = false;
+                        this.errorText = null;
 
-                if (tokens) {
+                        if (tokens) {
+                            localStorage.setItem('accessToken', tokens.data.access);
+                            localStorage.setItem('refreshToken', tokens.data.refresh);
+                            this.$store.commit('auth/AUTHENTICATE');
+                        }
 
-                    localStorage.setItem('accessToken', tokens.data.access);
-                    localStorage.setItem('refreshToken', tokens.data.refresh);
-                    this.$store.commit('auth/AUTHENTICATE');
+                        return this.$store.dispatch('auth/set_token', tokens.data)
+                    } catch(e) {
+                        this.isError = true;
+                        this.errorText = 'Неверное имя пользователя или пароль.'
+                    }
+                } else {
+                    if (this.murrenName === '') {
+                        this.error_container.username = 'Это поле обязательно';
+                    }
+                    if (this.password === '') {
+                        this.error_container.password = 'Это поле обязательно'
+                    }
                 }
-
-                return this.$store.dispatch('auth/set_token', tokens.data)
             },
         },
     }
 
 </script>
+
+<style>
+    .erorrHandling {
+        color: red;
+        text-align: center;
+
+        margin-bottom: 10px;
+    }
+</style>
--- src/components/account/SignUp.vue
+++ src/components/account/SignUp.vue
@@ -6,37 +6,29 @@
 
         <form class="form"
               v-on:submit.prevent="murrenSignup">
-
-            <input type="text"
-                   id="murren_name"
-                   v-bind:class="murren_name_form_class"
-                   v-model="murrenName"
-                   placeholder="Имя Муррена">
-
+            <MurrInput  
+                type="text" 
+                placeholder="Имя Муррена" 
+                :isError="error_container.hasOwnProperty('username')"
+                :errorText=" error_container.hasOwnProperty('username') ? error_container.username : null " 
+                @input="val => murrenName = val" />
             <br>
 
-            <input type="email"
-                   id="email"
-                   v-bind:class="murren_email_form_class"
-                   v-model="murrenEmail"
-                   placeholder="Почта">
-
+            <MurrInput  
+                type="email" 
+                placeholder="Почта" 
+                :isError="error_container.hasOwnProperty('email')"
+                :errorText=" error_container.hasOwnProperty('email') ? error_container.email : null " 
+                @input="val => murrenEmail = val" />
             <br>
-
-            <input type="password"
-                   id="password"
-                   v-bind:class="password_form_class"
-                   v-model="password"
-                   placeholder="Пароль">
-
-            <div
-                    v-if="input_have_error"
-                    class="container__error-text">
-
-                <p v-for="(error, index) in error_text"
-                   :key="index">
-                    {{ error }}</p>
-            </div>
+            
+            <MurrInput  
+                type="password" 
+                placeholder="Пароль" 
+                :isError="error_container.hasOwnProperty('password')"
+                :errorText=" error_container.hasOwnProperty('password') ? error_container.password : null " 
+                @input="val => password = val" />
+            <br>
 
             <br>
 
@@ -51,26 +43,15 @@
 
 </template>
 
-<style>
-
-    .error {
-        border: #ff0a36 10px solid;
-    }
-
-    .container__error-text {
-        margin: 0 auto;
-        background-color: pink;
-        max-width: 300px;
-        color: #0072ff;
-    }
-
-</style>
-
 <script>
 
-    import axios from 'axios'
+    import axios from 'axios';
+    import MurrInput from '@/components/Input';
 
     export default {
+        components: {
+            MurrInput
+        },
 
         data() {
 
@@ -82,32 +63,11 @@
 
 
                 murr_back_errors: '',
-
-                error_text: [],
-
-                murren_name_form_error: false,
-                murren_email_form_error: false,
-                password_form_error: false,
+                error_container: {},
 
                 input_have_error: false
             }
         },
-
-        watch: {
-
-            murren_name() {
-                this.reset_error_class_and_text();
-            },
-
-            murrenEmail() {
-                this.reset_error_class_and_text();
-            },
-
-            password() {
-                this.reset_error_class_and_text();
-            },
-        },
-
         methods: {
 
             async murrenSignup() {
@@ -125,24 +85,12 @@
 
                 } else {
 
-                    this.error_text = [];
+                    this.error_container = {};
 
                     for (let error_field in murr_back_response.data) {
-
-                        if (error_field === 'username') {
-
-                            this.murren_name_form_error = true;
-                            this.error_text.push(murr_back_response.data[error_field][0]);
-
-                        } else if (error_field === 'email') {
-
-                            this.murren_email_form_error = true;
-                            this.error_text.push(murr_back_response.data[error_field][0]);
-
-                        } else if (error_field === 'password') {
-
-                            this.password_form_error = true;
-                            this.error_text.push(murr_back_response.data[error_field][0]);
+                        this.error_container = {
+                            ...this.error_container,
+                            [error_field]: murr_back_response.data[error_field][0]
                         }
                     }
 
@@ -178,7 +126,22 @@
                     error: this.password_form_error,
                 }
             },
-        }
+        },
     }
 
 </script>
+
+<style>
+
+    .error {
+        border: #ff0a36 10px solid;
+    }
+
+    .container__error-text {
+        margin: 0 auto;
+        background-color: pink;
+        max-width: 300px;
+        color: #0072ff;
+    }
+
+</style>
